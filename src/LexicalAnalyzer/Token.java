9fb5ebc63429cd705d2ff30549f3ed9a46b46b10
151250209 <151250209@smail.nju.edu.cn>
2017-10-24 21:53:51 +0800

    update class Token

--- src/LexicalAnalyzer/Token.java
+++ src/LexicalAnalyzer/Token.java
@@ -1,4 +1,24 @@
 package LexicalAnalyzer;
 
 public class Token {
+    private int code;
+    private String str;
+    private String error;
+
+    public Token(int code, String str) {
+        this.code = code;
+        this.str = str;
+        this.error = null;
+    }
+
+    public Token(String error) {
+        this.error = error;
+    }
+
+    public String toString() {
+        if (this.error != null) {
+            return "Error:" + this.error;
+        }
+        return "<" + this.code + "," + this.str + ">";
+    }
 }
